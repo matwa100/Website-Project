733223b08d3df173d676123c15467a48e4043fb7
matwa100 <matwarren03@gmail.com>
2025-10-27 20:19:16 -0500

    navbar no routing

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './navbar'
+
 
 function App() {
   const [count, setCount] = useState(0)
-
-  return (
+  return ( 
     <>
+    <Navbar />
     <img src="/horse-horses.gif"></img>
       <h1>hello</h1>
       <div className="card">
